338321847bfca75112d56e9415b8b53d413fdc4d
a1ya1k2a9@gmail.com <a1ya1k2a9@gmail.com>
2023-11-26 07:24:44 -0800

    fixed package.json

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './index.css';
+import App from './App';
 
-const client = new ApolloClient({
-  uri: 'https://flyby-router-demo.herokuapp.com/',
-  cache: new InMemoryCache(),
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
-);
+// import ReactDOM from 'react-dom/client';
+// import App from './App.jsx';
+// import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+// import './index.css';
+
+// const client = new ApolloClient({
+//   uri: 'https://flyby-router-demo.herokuapp.com/',
+//   cache: new InMemoryCache(),
+// });
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <ApolloProvider client={client}>
+//     <App />
+//   </ApolloProvider>
+// );
